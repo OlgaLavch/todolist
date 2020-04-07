136e5d25f82febb866bd5fd0badd7ad55323b5b8
van0k13 <kazak13v@gmail.com>
2020-04-08 00:04:17 +0300

    Updates

---
+++ precache-manifest.99e78149bbe275a24b77f65d4163855f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299dcad1a184cff874bc57508e0750b0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "6324f693a111e7c9f178",
+    "url": "/todolist/static/css/main.c09deb88.chunk.css"
+  },
+  {
+    "revision": "851a4d108253dbb2aec6",
+    "url": "/todolist/static/js/2.99daaffd.chunk.js"
+  },
+  {
+    "revision": "6324f693a111e7c9f178",
+    "url": "/todolist/static/js/main.26304087.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
